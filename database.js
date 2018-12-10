544a71f7047401c02523981248c2a76e1a3334e2
Barbara <babagabrys@yahoo.co.uk>
2018-12-10 19:56:24 +0000

    fixed incorrect syntax in databade creation tables

--- database.js
+++ database.js
@@ -36,17 +36,17 @@ exports.createTables = function (conData, callback){
 		  database: conData.database
 		});
 		
-	let sql = "CREATE TABLE Users (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(32), password VARCHAR(16), firstName VARCHAR(16), lastName VARCHAR(16), email VARCHAR(40), registrationDate DATETIME, PRIMARY KEY (id))";
-	
-	sql += ";" + "CREATE TABLE Recipes (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(2048), authorId INT, desc LONGTEXT, ingredients LONGTEXT, createdDate DATETIME, photo VARCHAR(2048), keywords LONGTEXT, category VARCHAR(40), rating INT, views INT, status VARCHAR(40), PRIMARY KEY (id) )";
+	let sql = "CREATE TABLE Recipes (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(2048), authorId INT, description VARCHAR(3000), ingredients VARCHAR(3000), createdDate DATETIME, photo VARCHAR(2048), keywords VARCHAR(3000), category VARCHAR(40), rating INT, views INT, status VARCHAR(40), PRIMARY KEY (id))";
+
+	sql += ";" + "CREATE TABLE Users (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(32), password VARCHAR(16), firstName VARCHAR(16), lastName VARCHAR(16), email VARCHAR(40), registrationDate DATETIME, PRIMARY KEY (id))";
 	
 	sql += ";" + "CREATE TABLE Lists (id INT NOT NULL AUTO_INCREMENT, listname VARCHAR(40), recipeid INT, PRIMARY KEY (id) )";
 	
-	sql += ";" + "CREATE TABLE Steps (id INT NOT NULL AUTO_INCREMENT, recipeid INT, text LONGTEXT, photo VARCHAR(2048) PRIMARY KEY (id) )";
+	sql += ";" + "CREATE TABLE Steps (id INT NOT NULL AUTO_INCREMENT, recipeid INT, text VARCHAR(3000), photo VARCHAR(2048), PRIMARY KEY (id) )";
 
 	sql += ";" + "CREATE TABLE Subscriptions (id INT NOT NULL AUTO_INCREMENT, userId INT, creatorId INT, PRIMARY KEY (id) )";
 	
-	sql += ";" + "CREATE TABLE Comments (id INT NOT NULL AUTO_INCREMENT, authorId INT, recipeId INT, text LONGTEXT, commentParent INT, PRIMARY KEY (id) )";
+	sql += ";" + "CREATE TABLE Comments (id INT NOT NULL AUTO_INCREMENT, authorId INT, recipeId INT, text VARCHAR(3000), commentParent INT, PRIMARY KEY (id) )";
 
 	sql += ";" + "CREATE TABLE Test (id INT NOT NULL AUTO_INCREMENT, testField INT, PRIMARY KEY (id) )";
 
